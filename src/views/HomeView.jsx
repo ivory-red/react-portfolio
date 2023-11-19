e5ea1989e97f9d7e08473c6898d07b95843ca6cc
박상아 <sangahpark@bagsang-as-MacBook-Pro.local>
2023-11-20 01:42:46 +0900

    환경설정

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Main from "../components/Main";
+import Intro from "../components/Intro";
+
+const HomeView = () => {
+    return (
+        <>
+            <Header />
+            <Main>
+                <Intro />
+            </Main>
+            <Footer />
+        </>
+    )
+}
+
+export default HomeView;
