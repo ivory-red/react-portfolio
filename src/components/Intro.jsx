d29c41b9e70b4f21f3fc507e49e7d6fa9b06e3e1
SangAh Park <sept1st@naver.com>
2023-11-23 00:55:22 +0900

    minor changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Intro = () => {
-    return <div>Intro</div>
+    return (
+      <div className="intro t__center">
+        <h3 className="dotgothic16">front-end. web (developer)</h3>
+      </div>
+    );
 }
 
 export default Intro;
