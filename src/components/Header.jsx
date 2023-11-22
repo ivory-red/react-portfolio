4c80dc4e2add8a2c007b986d1c0fd20e357ae5a8
SangAh Park <sept1st@naver.com>
2023-11-23 00:16:47 +0900

    헤더 및 네비 작업 이미지 업로드

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+  {
+    title: "home",
+    url: "#intro"
+  },
+  {
+    title: "career",
+    url: "#career"
+  },
+  {
+    title: "edu",
+    url: "#edu"
+  },
+  {
+    title: "about",
+    url: "#about"
+  },
+  {
+    title: "contact",
+    url: "#contact"
+  }
+]
 
 const Header = () => {
-    return <div>Header</div>;
+  const [activeNav, setActive] = useState("home");
+
+  return (
+    <header id="header" role="banner">
+      <div className="header__inner inner">
+        <h1 className="logo">
+          <a href="#blank" className="manrope">spark.</a>
+        </h1>
+        <div className="header__info t__center">
+          <p>2yrs publisher</p>
+          <p>2yrs&4months front-end</p>
+        </div>
+      </div>
+      <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+        <ul>
+          {headerNav.map((nav, key) => (
+            <li key={key} className={activeNav === nav.title ? "active" : ""}>
+              <a href={nav.url} className="manrope">{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
 };
 
 export default Header;
