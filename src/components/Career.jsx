03b1c7344f4e8a5d8bb9cd711ee6f0c02fd8377e
SangAh Park <sept1st@naver.com>
2023-11-27 21:19:35 +0900

    sections career, edu, about 작업

---
+++ src/components/Career.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+// 이미지
+import arrow from "../assets/img/icons/ic_arrow.svg"
+import hanchon from "../assets/img/career/hanchon.svg"
+import kimgane from "../assets/img/career/kimgane.svg"
+import cheongnyun from "../assets/img/career/cheongnyun.svg"
+import dailybeer from "../assets/img/career/dailybeer.svg"
+import yeonan from "../assets/img/career/yeonan.svg"
+import sgfco from "../assets/img/career/sgfco.svg"
+
+const Career = () => {
+  return (
+    <div className="career section">
+      <div id="career" className="space"></div>
+      <div className="section__inner">
+        <div className="section__area career__area">
+          <h4 className="title manrope">career</h4>
+          <div className="career__content">
+            <div className="text__title__area mt__0">
+              <p className="career__title manrope">The Commerce <span>(프론트엔드 2021.03 - 2023.07)</span></p>
+              <a href="https://onesell.co.kr/" target="blank" className="arrow__link nanum">바로가기<img src={arrow} alt="화살표" /></a>
+            </div>
+            <div className="text__area nanum">
+              <p className="text__title">원셀 서비스 개발 (판매자들을 위한 쇼핑몰 연동 및 상품 등록/수정/전송, 배송/주문 관리 서비스 제공)</p>
+              <ul>
+                <li>
+                  서비스 반응형화 작업
+                </li>
+                <li>
+                  위탁몰 연동 및 판매 신규 기능 개발 (상품 수집 - 주문 관리)
+                </li>
+                <li>
+                  Vue.js, HTML, SCSS
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="career__content">
+            <div className="text__title__area">
+              <p className="career__title manrope">View3 <span>(웹 퍼블리셔 2017.09 - 2019.11)</span></p>
+              <a href="https://view3.net/" target="blank" className="arrow__link nanum">바로가기<img src={arrow} alt="화살표" /></a>
+            </div>
+
+            <div>
+              <div className="text__area nanum">
+                <p className="text__title">여러 프랜차이즈 PC 및 모바일 홈페이지 제작 (한촌, 김가네, 청년다방, 생활맥주, 연안식당, 세광그린푸드 등 담당)</p>
+                <ul>
+                  <li>
+                    PC / 모바일 버전 제작, 반응형 제작
+                  </li>
+                  <li>
+                    HTML, CSS, JavaScript, JQuery
+                  </li>
+                </ul>
+              </div>
+              <div className="swiper__area">
+                {/* <div className="swiper__area__hover">
+                  <div className="swipe">
+                    <p>swipe</p>
+                    <p className="manrope">
+                      <span><i>&lt;</i><i>&lt;</i><i>&lt;</i></span>
+                      <span><i>&gt;</i><i>&gt;</i><i>&gt;</i></span>
+                    </p>
+                  </div>
+                </div> */}
+                <Swiper
+                  spaceBetween={32}
+                  slidesPerView={3.5}
+                >
+                  <SwiperSlide>
+                    <div className="img__area">
+                      <img src={hanchon} alt="한촌" width="100%" />
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={kimgane} alt="" width="100%" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={cheongnyun} alt="청년다방" width="100%" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={dailybeer} alt="생활맥주" width="100%" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={yeonan} alt="연안식당" width="100%" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={sgfco} alt="세광그린푸드" width="100%" />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Career;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,65 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-scroll";
 
 const headerNav = [
   {
-    title: "home",
-    url: "#intro"
+    title: "home"
   },
   {
-    title: "career",
-    url: "#career"
+    title: "career"
   },
   {
-    title: "edu",
-    url: "#edu"
+    title: "edu"
   },
   {
-    title: "about",
-    url: "#about"
-  },
-  {
-    title: "contact",
-    url: "#contact"
+    title: "about"
   }
 ]
 
 const Header = () => {
-  const [activeNav, setActive] = useState("home");
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const setNavActive = (title) => {
-    setActive(title)
-  };
-
-  const toggleMode = () => {
-    setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode);
-  };
-
   return (
-    <header id="header" role="banner">
-      <div className="header__inner inner">
-        <h1 className="logo">
-          <a href="#blank" className="manrope">spark.</a>
-        </h1>
-        <div className="header__info t__center">
-          <p>2yrs publisher</p>
-          <p>2yrs&4months front-end</p>
+    <>
+      <header id="header" role="banner">
+        <div className="header__inner inner">
+          <h1 className="logo">
+            <a href="#blank" className="manrope">spark.</a>
+          </h1>
+          <div className="header__info t__center">
+            <p>2yrs publisher</p>
+            <p>2yrs&4months front-end</p>
+          </div>
         </div>
-      </div>
+      </header>
       <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
         <ul>
           {headerNav.map((nav, key) => (
-            <li key={key} className={activeNav === nav.title ? "active" : ""}>
+            <li key={key}>
               {/* 화살표 함수로 래핑함으로써 해당 함수는 클릭 할 떄만 변할 수 있도록 설정 */}
-              <a href="#none" className="manrope" onClick={() => setNavActive(nav.title)}>{nav.title}</a>
+              <Link className="manrope" activeClass="active" to={nav.title} spy={true} smooth={true}>
+                {nav.title}
+              </Link>
             </li>
           ))}
         </ul>
       </nav>
-      <div className="mode">
-        <a href="#none" className="manrope" onClick={toggleMode}>{isDarkMode === false ? "dark" : "light"} mode.</a>
-      </div>
-    </header>
+    </>
   )
 };
 
